936ad56ab38a82774af72dfb358c7339c320f16d
Nishit Anand <44223079+nishitanand@users.noreply.github.com>
2021-01-07 22:02:51 +0530

    Updated twilio_main.py

--- python_code_files/twilio_main.py
+++ python_code_files/twilio_main.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client 
  
-account_sid = 'ACaf4562a0b38683a390fe511629c2e711' 
-auth_token = '635be73e7dd8b31e507d318c8eda430e' 
+account_sid = '' 
+auth_token = '' 
 client = Client(account_sid, auth_token) 
 
 if (int(d['geeta:'])<100 and int(d['niyati:'])<100):
@@ -17,38 +17,38 @@ if (int(d['geeta:'])<100 and int(d['niyati:'])<100):
 
     if (gun==True and knife==True):
         message = client.messages.create( 
-                                    from_='+12515125151', 
+                                    from_='', 
                                     body='EMERGENCY ALERT!! GUN and KNIFE Detected...Authorities have been notified',       
-                                    to='+919899218301' 
+                                    to='' 
                                 ) 
         message = client.messages.create( 
-                                    from_='whatsapp:+14155238886', 
+                                    from_='', 
                                     body='EMERGENCY ALERT!! GUN and KNIFE Detected...Authorities have been notified',       
-                                    to='whatsapp:+919899218301' 
+                                    to='' 
                                 )
 
     if (gun==True and knife==False):
         message = client.messages.create( 
-                                    from_='+12515125151', 
+                                    from_='', 
                                     body='EMERGENCY ALERT!! GUN Detected...Authorities have been notifiedd',       
-                                    to='+919899218301' 
+                                    to='' 
                                 ) 
         message = client.messages.create( 
-                                    from_='whatsapp:+14155238886', 
+                                    from_='', 
                                     body='EMERGENCY ALERT!! GUN Detected...Authorities have been notified',       
-                                    to='whatsapp:+919899218301' 
+                                    to='' 
                                 )
 
     if (gun==False and knife==True):
         message = client.messages.create( 
-                                    from_='+12515125151', 
+                                    from_='', 
                                     body='EMERGENCY ALERT!! KNIFE Detected...Authorities have been notified',       
-                                    to='+919899218301' 
+                                    to='' 
                                 ) 
         message = client.messages.create( 
-                                    from_='whatsapp:+14155238886', 
+                                    from_='', 
                                     body='EMERGENCY ALERT!! KNIFE Detected...Authorities have been notified',       
-                                    to='whatsapp:+919899218301' 
+                                    to='' 
                                 )
     
     print(message.sid)
